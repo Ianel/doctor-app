8ba5dab829b8b0bd3b61ac1d2d11dbc56902013f
Ianel Tombozafy <ianell.s118@gmail.com>
2022-02-07 07:18:38 +0300

    bugfix: Bottom navbar is now responsive

--- src/components/header/NavDown.js
+++ src/components/header/NavDown.js
@@ -3,31 +3,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 
 const NavDown = () => {
-    const [isDisplayed, setIsDisplayed] = useState(true);
+    const [isClicked, setIsClicked] = useState(false);
 
   return (
-
-    <nav>
-       { isDisplayed ?
+    <>
+        <div className={`flex justify-between items-center py-3 px-4 lg:hidden ${isClicked ? 'shadow-none' : 'shadow'}`}>
+            <h1 className='text-xl font-bold'>M<span className='text-[#516191]'>e</span>di<span className='text-pink-700'>k</span>Ai<span className='text-pink-400'>d</span></h1>
+            { isClicked ? 
+            <FontAwesomeIcon className='cursor-pointer p-1 text-4xl lg:hidden' icon={faTimes} onClick={ () => setIsClicked(isClicked => !isClicked) } />
+            :
+            <FontAwesomeIcon className='cursor-pointer p-2 text-4xl lg:hidden' icon={faBars} onClick={ () => setIsClicked(isClicked => !isClicked) } />
+            }
+        </div>
+        {
+            isClicked ?
             (
-                <div className='shadow-md flex transition-all bg-white text-[#516191] lg:flex flex-col lg:flex-row p-4 lg:justify-center lg:items-center'>
-                    <div className='border-b-2 py-3 w-full lg:w-auto lg:border-none'>
-                        <a href="#hero" className='hover:text-pink-700 lg:p-2 uppercase'>Accueil</a>
-                        <button onClick={() => { setIsDisplayed(false); }} className='float-right lg:hidden'><FontAwesomeIcon icon={faTimes} /></button>
-                    </div>
-                    <a href="#bio" className='w-full py-3 border-b-2 lg:w-auto lg:border-none hover:text-pink-700 lg:p-2 uppercase'>Apropos</a>
-                    <a href="#services" className='w-full py-3 lg:w-auto lg:border-none hover:text-pink-700 lg:p-2 uppercase'>Services</a>
+                <div className='w-full lg:hidden px-4 shadow flex-col justify-evenly items-start leading-8 lg:gap-10 flex'>
+                    <a className='hover:text-pink-700 py-2 text-[#516191] uppercase border-b-2 w-full text-primary hover:text-secondary' href="#hero">Accueil</a>
+                    <a className='hover:text-pink-700 py-2 text-[#516191] uppercase border-b-2 w-full text-primary hover:text-secondary' href="#bio">A propos</a>
+                    <a className='hover:text-pink-700 py-2 text-[#516191] uppercase w-full text-primary hover:text-secondary' href="#services">Services</a>
                 </div>
             )
             :
             (
-                <div className='p-4 flex justify-between items-center bg-white transition-all'>
-                    <h1 className='text-xl font-bold'>M<span className='text-[#516191]'>e</span>di<span className='text-pink-700'>k</span>Ai<span className='text-pink-400'>d</span></h1>
-                    <button onClick={() => { setIsDisplayed(true); }}><FontAwesomeIcon icon={faBars} /></button>
-                </div>
+                <nav className='hidden lg:flex lg:flex-row lg:justify-center lg:items-center py-3 px-8 shadow-md'>
+                    <div className='flex-col justify-evenly items-start lg:justify-between lg:items-center lg:flex-row leading-8 lg:gap-10 lg:flex'>
+                        <a className='hover:text-pink-700 py-2 text-[#516191] uppercase' href="#hero">Accueil</a>
+                        <a className='hover:text-pink-700 py-2 text-[#516191] uppercase' href="#bio">A propos</a>
+                        <a className='hover:text-pink-700 py-2 text-[#516191] uppercase' href="#services">Services</a>
+                    </div>
+                </nav>
             )
         }
-    </nav>
+    </>
   );
 };
 
